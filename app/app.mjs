94382c18933bde1ee957861052afbc8476b434cc
drvnprgrmr <ekunweibukun159357@gmail.com>
2022-07-21 11:00:53 +0100

    Added  images

---
+++ app/app.mjs
@@ -0,0 +1,65 @@
+function startApp() {
+    // Your entire app should not necessarily be coded inside this
+    // single function (though there's no penalty for that),
+    // so create and use/call additional functions from here
+
+    // pls remove the below and make some magic in here!
+    console.log("make magic in here!");
+
+    const header = document.querySelector("h2");
+    if (header) {
+        header.textContent = "make some magic here!!";
+    }
+}
+
+let carrierPrefixes = {
+    0701: "airtel",
+    07020: "smile",
+    07025: "mtn",
+    07026: "mtn",
+    07027: "multi-links",
+    07028: "starcomms",
+    07029: "starcomms",
+    0703: "mtn",
+    0704: "mtn",
+    0705: "glo",
+    0706: "mtn",
+    0707: "zoom-mobile",
+    0708: "airtel",
+    0709: "multi-links",
+    0802: "airtel",
+    0803: "mtn",
+    0804: "mtel",
+    0805: "glo",
+    0806: "mtn",
+    0807: "glo",
+    0808: "airtel",
+    0809: "9mobile",
+    0810: "mtn",
+    0811: "glo",
+    0812: "airtel",
+    0813: "mtn",
+    0814: "mtn",
+    0815: "glo",
+    0816: "mtn",
+    0817: "9mobile",
+    0818: "9mobile",
+    0819: "starcomms",
+    0909: "9mobile",
+    0908: "9mobile",
+    0901: "airtel",
+    0902: "airtel",
+    0903: "mtn",
+    0904: "airtel",
+    0905: "glo",
+    0906: "mtn",
+    0907: "airtel",
+    0915: "glo",
+    0913: "mtn",
+    0912: "airtel",
+    0916: "mtn",
+};
+
+// ======= DO NOT EDIT ============== //
+export default startApp;
+// ======= EEND DO NOT EDIT ========= //
